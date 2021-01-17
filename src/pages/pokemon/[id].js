2610d021d7c06267de0e35205e64be682785411f
JalimRabey <123risada@gmail.com>
2021-01-16 23:14:07 -0300

    update to use list in pokemon types

--- src/pages/pokemon/[id].js
+++ src/pages/pokemon/[id].js
@@ -40,9 +40,26 @@ const Pokemon = ({ name, imageUrl, weight, height, types }) => {
 
         <h2 className="text-2xl mt-6 mb-2 font-bold">Types</h2>
 
-        {types.map((type) => (
-          <p key={type.type.name}>{type.type.name}</p>
-        ))}
+        <ul>
+          {types.map((type) => (
+            <li key={type.type.name} className="flex items-center mt-2">
+              <span class="h-6 flex items-center sm:h-7">
+                <svg
+                  class="flex-shrink-0 h-5 w-5 text-cyan-500"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+              <p className="ml-1">{type.type.name}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </Layout>
   );
